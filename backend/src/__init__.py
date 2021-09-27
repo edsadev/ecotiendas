e2bddb24e6d2b4f9f42785fa5a7cd727b47ffab9
Jamil Torres <42499083+jatobrun@users.noreply.github.com>
2021-09-27 07:45:53 -0500

    sidecar patern

--- backend/src/__init__.py
+++ backend/src/__init__.py
@@ -4,7 +4,7 @@ from flask_cors import CORS
 from flask_migrate import Migrate
 import os
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:tbwIr0Bj4eb1Jzov@34.123.179.176:5432/ecotiendas' 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:tbwIr0Bj4eb1Jzov@127.0.0.1:5432/ecotiendas' 
 root_path = os.path.dirname(os.path.abspath(__file__))
 app.config['UPLOAD_FOLDER'] = os.path.join(root_path, 'reportes')
 db = SQLAlchemy(app)
