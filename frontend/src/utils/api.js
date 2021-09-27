32eaaf84dd6de98815eb9692b4a68ea461bf38c5
Jamil Torres <42499083+jatobrun@users.noreply.github.com>
2021-09-26 21:14:28 -0500

    hello world function

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -58,7 +58,7 @@ export async function getInitialDataEcoAdmin(){
 export function helloworld(){
   return axios({
     method: 'get',
-    url: `${ipUrl}${puerto}/`,
+    url: `${ipUrl}${puerto}`,
   })
 }
 function getMaterials(){
