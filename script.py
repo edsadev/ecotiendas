3f31b523060b757f6d981d61e5e64b2e0efff408
Jamil Torres <42499083+jatobrun@users.noreply.github.com>
2021-09-27 11:04:18 -0500

    port frontend

--- script.py
+++ script.py
@@ -6,9 +6,9 @@ import time
 url = 'http://localhost:5000/record'
 headers = {'content-type': 'application/json'}
 
-with serial.Serial('/dev/cu.usbserial-140', 9600, timeout=1) as ser: 
-    # print(f'Antes de enviar comando \n{ser.readline().decode("utf-8")}')
-    # ser.write(b'R')      
+with serial.Serial('/dev/cu.usbserial-1240', 9600, timeout=1) as ser: 
+    print(f'Antes de enviar comando \n{ser.readline().decode("utf-8")}')
+    ser.write(b'R')      
     peso = ser.readline().decode("utf-8")
     print(f'despues de enviar el comando {peso}')
     # data = {
