fa97395800aaa7889c825a026f268e3e519f48cc
Jamil Torres <42499083+jatobrun@users.noreply.github.com>
2021-09-30 11:08:31 -0500

    change in email

--- script-tscale.py
+++ script-tscale.py
@@ -3,31 +3,28 @@ import requests
 import json
 import time
 
-url = 'http://localhost:5000/record'
+url = 'http://192.168.36.224:5000/record'
 headers = {'content-type': 'application/json'}
 puerto = '/dev/cu.usbserial-1240'
-baud_rate = 9600
-pesos = []
-contador = 0
+baud_rate = 1200
+
 while True:
 
     with serial.Serial(puerto, baud_rate, timeout=1) as ser:
-        peso = ser.readline().decode("utf-8").split(' ')[4].strip()[:-2]
-        print(peso)
-        if peso in pesos and peso != '0.00':
-            contador +=1
-        else:
-            pesos.append(peso)
-        
-        if contador >= 200:
+        data = ser.readline().decode('latin-1').split(' ')
+        if len(data) > 4:
+            peso = data[4].strip()
+            if 'kg' in peso:
+                peso = peso[:-2]
 
-            data = {
+            request = {
                 'ecotienda': 1,
                 'peso': peso
             }
-            r = requests.post(url = url, data=json.dumps(data), headers=headers)
-            contador = 0
-            pesos = []
+            print(request)
+            r = requests.post(url = url, data=json.dumps(request), headers=headers)
+            time.sleep(3)
+            
 
 
 
