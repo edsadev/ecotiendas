4bae6492d2928eed2ddbd3f00f0fa1d1d6ce0c5e
Jamil Torres <42499083+jatobrun@users.noreply.github.com>
2021-11-17 08:55:31 -0500

    prueba.py send email

--- backend/src/prueba.py
+++ backend/src/prueba.py
@@ -36,7 +36,7 @@ def enviar_email(correo, ecopuntos, total_ecopuntos):
 
     port = 465  # For SSL
     smtp_server = "smtp.gmail.com"
-    ontext = ssl.create_default_context()
+    context = ssl.create_default_context()
     with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
         server.login(msg['From'], password)
         server.sendmail(msg['From'], msg['To'], msg.as_string())
